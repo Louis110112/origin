e37f255a9cfcb7f67dfb5bae57896fcf3c466f08
louis93507@gmail.com <louis93507@gmail.com>
2024-09-24 14:20:51 +0800

    build:route & controller setup

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-    res.send('express for movies')
+    res.redirect('/movies')
 })
 
+app.get('/movies', (req, res) => {
+    res.send('lisiting movies')
+})
+
+app.get('/movie/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`read movies${id}`)
+
+})
 app.listen(port, () => {
     console.log(` express server is running on http://localhost:${port}`)
 })
