cee0a8d8585a562e75f88452e2194a9dfd39b772
louis93507@gmail.com <louis93507@gmail.com>
2024-09-25 11:30:42 +0800

    "Resolve merge conflicts from stash pop"

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express")
+const { engine } = require("express-handlebars")
 const app = express()
 const port = 3001
+
+
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
     res.redirect('/movies')
 })
 
 app.get('/movies', (req, res) => {
-    res.send('lisiting movies')
+    res.render('index')
 })
 
 app.get('/movie/:id', (req, res) => {
